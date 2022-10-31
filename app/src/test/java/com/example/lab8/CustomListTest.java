1009916e841b993d7a9720d9f39b7f34a0fbaeba
Dhami0007 <114706870+Dhami0007@users.noreply.github.com>
2022-10-31 15:48:47 -0600

    Update CustomListTest.java
    testing functionalities

--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -12,5 +12,59 @@ import java.util.ArrayList;
 
 public class CustomListTest {
 
+    private CustomList list;
+    /**
+     * create a mocklist for my citylist
+     * @return
+     */
+    public CustomList MockCityList(){
+        list = new CustomList(null,new ArrayList<>());
+        return list;
+    }
 
+    /**
+     * get the size of the list
+     * increase the list by adding a new city
+     * check if our current size matches the initial size plus
+     one
+     */
+    @Test
+    public void addCityTest(){
+        list = MockCityList();
+        int listSize = list.getCount();
+        list.addCity(new City("Estevan", "SK"));
+        assertEquals(list.getCount(),listSize + 1);
+    }
+
+
+    /**
+     * get the size of the list
+     * decrease the list by delete an existing city
+     * check if our current size matches the initial size plus
+     one
+     */
+    @Test
+    public void deleteCityTest(){
+        list = MockCityList();
+        int listSize = list.getCount();
+        list.addCity(new City("Estevan", "SK"));
+        list.deleteCity(new City("Estevan", "SK"));
+        assertEquals(list.getCount(),listSize);
+    }
+
+
+    /**
+     * get the size of the list
+     * increase the list by adding a new city
+     * check if our current size matches the initial size plus
+     one
+     */
+    @Test
+    public void hasCityTest(){
+        list = MockCityList();
+        int listSize = list.getCount();
+        list.addCity(new City("Estevan", "SK"));
+        boolean  value = list.hasCity(new City("Estevan", "SK"));
+        assertEquals(value,true);
+    }
 }
